7368a0aa3d942a6ecf1f1feb09014f6c1c99bd8b
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-05-18 08:24:13 -0700

    refactor: add image

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="w-1520 flex justify-between items-center pb-23 border-b-2 border-black">
-    <img class="h-[24px] w-[147.8px]" src="https://i.ibb.co/m0D0zkG/Frame.png" />
+    <img class="h-[24px] w-[147.8px]" :src="frame" />
     <p class="text-black font-bold text-[40px]">{{ page }}/4</p>
   </div>
 </template>
 
 <script>
+import frame from "../../assets/images/Frame.png"
 export default {
-  props: ['page']
+  props: ['page'],
+  data () {
+    return {
+      frame
+    }
+  },
 }
 </script>
